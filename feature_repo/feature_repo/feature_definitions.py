ce44766032fea9a1fcec2671120a98ce84a13b4c
aparnakrishnan21 <aparnakrishnan21@gmail.com>
2026-07-20 13:53:16 +0530

    Updated Feast Feature Store

--- feature_repo/feature_repo/feature_definitions.py
+++ feature_repo/feature_repo/feature_definitions.py
@@ -1,23 +1,40 @@
-from feast import FeatureView, FileSource, Field
-from feast.types import Float32
 from datetime import timedelta
 
+from feast import Entity, FeatureView, Field
+from feast.infra.offline_stores.file_source import FileSource
+from feast.types import String, Int64, Float32
 
-house_source = FileSource(
-    path="data/features.parquet",
-    timestamp_field="event_timestamp"
+# Entity
+property_entity = Entity(
+    name="property_id",
+    join_keys=["property_id"],
 )
 
+# Data source
+house_source = FileSource(
+    name="house_source",
+    path="data/features.parquet",
+    timestamp_field="event_timestamp",
+)
 
+# Feature View
 house_features = FeatureView(
     name="house_features",
-    entities=[],
+    entities=[property_entity],
     ttl=timedelta(days=365),
     schema=[
-        Field(
-            name="feature_name",
-            dtype=Float32
-        ),
+        Field(name="property_type", dtype=String),
+        Field(name="location", dtype=String),
+        Field(name="city", dtype=String),
+        Field(name="province_name", dtype=String),
+        Field(name="latitude", dtype=Float32),
+        Field(name="longitude", dtype=Float32),
+        Field(name="baths", dtype=Int64),
+        Field(name="purpose", dtype=String),
+        Field(name="bedrooms", dtype=Int64),
+        Field(name="Area Type", dtype=String),
+        Field(name="Area Size", dtype=Float32),
+        Field(name="Area Category", dtype=String),
     ],
     source=house_source,
 )
